4d25c76537dfd00de76c07a4c16532d244f6eb0e
chandanv-a11y <chandan.v@decathlon.com>
2026-01-07 15:11:28 +0530

    supplier pannel added x2

--- supplier.js
+++ supplier.js
@@ -3,9 +3,9 @@ const SUPPLIER_GID = "678738705";
 const ITEM_GID = "1062614252";
 
 const SUPPLIER_URL =
-`https://docs.google.com/spreadsheets/d/e/2PACX-1vRUKuo3yzsWt5nrvL0h6fnnX8DjwTyzc9VzTi9VmdanF-DZChVDVrJrtJ55THxWzp78KJcMdURoLGe9/pub?output=csv&gid=${SHEET_GID}`
+`https://docs.google.com/spreadsheets/d/e/2PACX-1vRUKuo3yzsWt5nrvL0h6fnnX8DjwTyzc9VzTi9VmdanF-DZChVDVrJrtJ55THxWzp78KJcMdURoLGe9/pub?output=csv&gid=${SUPPLIER_GID}`
 const ITEM_URL =
-`https://docs.google.com/spreadsheets/d/e/2PACX-1vRUKuo3yzsWt5nrvL0h6fnnX8DjwTyzc9VzTi9VmdanF-DZChVDVrJrtJ55THxWzp78KJcMdURoLGe9/pub?output=csv&gid=${SHEET_GID}`
+`https://docs.google.com/spreadsheets/d/e/2PACX-1vRUKuo3yzsWt5nrvL0h6fnnX8DjwTyzc9VzTi9VmdanF-DZChVDVrJrtJ55THxWzp78KJcMdURoLGe9/pub?output=csv&gid=${ITEM_GID}`
   
 let supplierMaster = [];
 let itemData = [];
